2acc2e141ff2f12b89bcf6fa83b3b55bec17a2f9
7imac16 <sato@seven-inc.co.jp>
2017-04-21 13:57:01 +0900

    career | PCレイアウト完了

---
+++ recruit/career/_inc/otherStory.php
@@ -0,0 +1,29 @@
+ <?php
+ /**
+ * $activePerson { str } アクティブなメンバーを指定
+ * $isChild{boolean} Person第２階層まで対応
+ */
+    $path = $isChild ? '../' : '';
+
+    if( $activePerson == 'hayashi' ){$hayashiIsActive = true;}
+        elseif( $activePerson == 'nishigaki' ){$nishigakiIsActive = true;}
+        elseif( $activePerson == 'mizutani' ){$mizutaniIsActive = true;}
+ ?>
+ <aside class="otherStory">
+    <header class="otherStory_index">
+        OTHER STORY
+    </header>
+    <nav class="otherStory_nav">
+	    <ul>
+	        <li<?php if( isset($hayashiIsActive)) echo ' class="current"' ?>>
+	            <a <?php if( !isset($hayashiIsActive) ) echo 'href="'. $path . 'hayashi/"' ?>><img src="<?php echo $path; ?>img/index-img-hayashi.jpg" alt=""></a>
+	        </li>
+	        <li<?php if( isset($nishigakiIsActive)) echo ' class="current"' ?>>
+	            <a <?php if( !isset($nishigakiIsActive) ) echo 'href="'. $path . 'nishigaki/"' ?>><img src="<?php echo $path; ?>img/index-img-nishigaki.jpg" alt=""></a>
+	        </li>
+	        <li<?php if( isset($mizutaniIsActive)) echo ' class="current"' ?>>
+	            <a <?php if( !isset($mizutaniIsActive) ) echo 'href="'. $path . 'hayashi/"' ?>><img src="<?php echo $path; ?>img/index-img-mizutani.jpg" alt=""></a>
+	        </li>
+	    </ul>
+    </nav>
+</aside>
