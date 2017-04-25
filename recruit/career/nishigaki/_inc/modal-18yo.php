07c45200cbf797b48acb82379318e466fe5bbd14
7imac16 <sato@seven-inc.co.jp>
2017-04-25 21:12:02 +0900

    career | 西垣さんレイアウト、モーダル

---
+++ recruit/career/nishigaki/_inc/modal-18yo.php
@@ -0,0 +1,27 @@
+
+ <div id="modal-18yo" class="career_modal career_modal-future modal">
+    <div class="wrap">
+      <header class="careerModal_header">
+         <span>18</span>
+         <img src="../img/yo.svg" width="140" class="yo" alt="Years Old">
+      </header>
+      <div class="careerModal_content">
+        <div class="careerModal_content_index">
+          “それは間違っている”――<br>
+          監督のひとことに<br>
+          10年先を見ることを学んだ。
+
+        </div>
+        <div class="careerModal_content_text">
+          ４年に上がる直前、監督に呼び出され、こう聞かれた。「目標は何だ？」と。私は「全国の上位８位以内に入ることです」と答えた。しばらくして監督はこう言った。「それは間違っている。大学日本一こそ目標だろう」と。私は、自分が最上級生になる年の現実的な目標を答えたに過ぎなかったのだ。しかし、水泳部はすでに100年を超える歴史をもち、これからも歩みを重ねて行く。目標は確かに日本一でなければならなかった。その後、私は陸上に上がり専任コーチになることを決意した。この1年ではなく10年先を見れば、当時水泳部に必要なのは選手兼任ではない専任コーチの存在だった。
+        </div>
+      </div>
+      <figure class="careerModal_footerImg">
+        <img src="img/modal-future-img.jpg" width="770" alt="">
+        <i class="careerModal_footerImg_bg"></i>
+      </figure>
+
+      <div class="modal_close"><span>close</span><i></i></div>      
+      
+    </div>
+ </div>
---
+++ recruit/career/nishigaki/_inc/modals.php
@@ -0,0 +1,10 @@
+<div class="modals careerModals">
+  <?php 
+    include 'modal-12yo.php';
+    include 'modal-18yo.php';
+    include 'modal-23yo.php';
+    include 'modal-last.php';
+
+  ?>
+  
+</div>
