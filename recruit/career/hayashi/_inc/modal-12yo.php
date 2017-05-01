7d2455ff165dc80549e9f6dab6f952e969a8c03d
7imac16 <sato@seven-inc.co.jp>
2017-05-01 12:29:16 +0900

    career | 林さん追加

---
+++ recruit/career/hayashi/_inc/modal-12yo.php
@@ -0,0 +1,27 @@
+
+ <div id="modal-12yo" class="career_modal career_modal-future modal">
+    <div class="wrap">
+      <header class="careerModal_header">
+         <span>12</span>
+         <img src="../img/yo.svg" width="140" class="yo" alt="Years Old">
+      </header>
+      <div class="careerModal_content">
+        <div class="careerModal_content_index">
+          水泳を通して、個とチーム<br>
+          それぞれの視点が必要だと<br>
+          いうことを学んだ。
+
+        </div>
+        <div class="careerModal_content_text">
+          多くの大会に出た。大会では１位８点、２位７点、というように個人の順位によってチームに点が入り、その合計でチームの順位を競う。そのため水泳には、個人競技であると同時にチームスポーツという面があった。チームが“のった”ときには個人の成績も出る。チームとしてコンディションやモチベーションをいかに高めるのか、個とチームという２つの視点が必要だった。その発見は、今も生きている。
+        </div>
+      </div>
+      <figure class="careerModal_footerImg">
+        <img src="img/modal-future-img.jpg" width="770" alt="">
+        <i class="careerModal_footerImg_bg"></i>
+      </figure>
+
+      <div class="modal_close"><span>close</span><i></i></div>      
+      
+    </div>
+ </div>
