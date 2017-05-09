114bbdfefc1f0e6e42b9b41d6053c24b88dce90b
7imac16 <sato@seven-inc.co.jp>
2017-05-09 16:57:15 +0900

    WJU ｜ モーダル調整

--- recruit/why_join_us/_inc/modal-07.php
+++ recruit/why_join_us/_inc/modal-07.php
@@ -14,7 +14,11 @@
                         <span>自己成長の意欲が、<br>評価の大きなポイントになります！</span>
                     </div>
                     <div class="qaSection_content">
-                        <p>急速に成長している当社では、自身を高めていきたいという想いを強く持って、主体的に動いていくことができる方かどうかが評価のポイントになります。また、当社は業界のリーダー企業となるだけではなく、業界そのものを変え、社会を変えていくことをビジョンにかかげています。そんな自分と会社のビジョンに向かって突き進んで、成長を加速させたいという気持ちを面接で伝えてください。詳細は当社の求める人物像のページをご参照ください。<br><a href="../recruiting/walfare.html">http://www.propertyagent.co.jp/recruit/recruiting/walfare.html</a></p>
+                        <p>急速に成長している当社では、自身を高めていきたいという想いを強く持って、主体的に動いていくことができる方かどうかが評価のポイントになります。また、当社は業界のリーダー企業となるだけではなく、業界そのものを変え、社会を変えていくことをビジョンにかかげています。そんな自分と会社のビジョンに向かって突き進んで、成長を加速させたいという気持ちを面接で伝えてください。詳細は当社の求める人物像のページをご参照ください。
+                        </p>
+                        <p>
+                            <small><a href="../recruiting/walfare.html">http://www.propertyagent.co.jp/recruit/recruiting/walfare.html</a></small>
+                        </p>
                     </div>
                     <div class="qaSection_footerImg">
                         <img src="img/modal07-a-img.jpg" width="315" alt="">
