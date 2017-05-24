c755911e15decca452dfa045b7e941cc9a0b3fe4
7imac16 <sato@seven-inc.co.jp>
2017-05-24 20:43:47 +0900

    business | 微修正

--- recruit/business/_inc/localNav.php
+++ recruit/business/_inc/localNav.php
@@ -10,7 +10,7 @@
   <div class="wrap">
     <nav class="careerSelectNav">
       <ul>
-        <li class="careerSelectNav_vision<?php if( isset( $visionIsActive ) ) echo' active'; ?>" data-genre="vision"><a <?php if( ! isset($visionIsActive) ) echo 'href="/"'; ?>>VISION</a></li>
+        <li class="careerSelectNav_vision<?php if( isset( $visionIsActive ) ) echo' active'; ?>" data-genre="vision"><a <?php if( ! isset($visionIsActive) ) echo 'href="index.html"'; ?>>VISION</a></li>
         <li class="careerSelectNav_about<?php if( isset( $aboutIsActive ) ) echo' active'; ?>" data-genre="about"><a <?php if( ! isset($aboutIsActive) ) echo 'href="about.html" target="_blank"'; ?>>ABOUT</a></li>
       </ul>
     </nav>
